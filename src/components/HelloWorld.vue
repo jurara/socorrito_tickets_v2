58fa4498753a12cdc3590a1ddd170c9f87b689cd
jurara <juraramirezga@ittepic.edu.mx>
2024-03-22 13:47:48 -0600

    primer commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,226 @@
+<template>
+  <v-container>
+    <v-col md="12">
+      <v-row justify='end' style="background-color: white">
+        <v-col cols="12" sm="6" md="4">
+          <v-text-field elevation="8" label="Buscar" hint="Escribe el nombre del producto a buscar" v-model="search"
+            clearable variant="outlined"></v-text-field>
+          
+        </v-col>
+        <v-col cols="12" sm="3" md="1">
+          <v-dialog v-model="dialog" persistent width="1024">
+              <template v-slot:activator="{ props }">
+                <v-btn color="primary" v-bind="props">
+                  Ticket
+                </v-btn>
+              </template>
+              <v-card>
+                <v-card-title>
+                  <span class="text-h5">Crear ticket personalizado</span>
+                </v-card-title>
+                <v-card-text>
+                  <v-container>
+                    <v-row>
+                      <v-col cols="12" sm="6" md="4">
+                        <v-text-field label="Producto" required v-model="producto"></v-text-field>
+                      </v-col>
+                      <v-col cols="12" sm="6" md="4">
+                        <v-text-field label="Gramaje" required v-model="gramaje"></v-text-field>
+                      </v-col>
+                      <v-col cols="12" sm="6" md="4">
+                        <v-text-field label="Codigo de barras / QR" required v-model="codigo"></v-text-field>
+                      </v-col>
+
+                    </v-row>
+                  </v-container>
+                  <small>*Recuerda que si este producto no existe deberas registrarlo en tu sistema de ventas para poder
+                    utilizar este QR</small>
+                </v-card-text>
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn color="blue-darken-1" variant="text" @click="dialog = false">
+                    Cancelar
+                  </v-btn>
+                  <v-btn color="blue-darken-1" variant="text" @click="modalPDF">
+                    Crear
+                  </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
+          
+        </v-col>
+
+      </v-row>
+    </v-col>
+
+
+    <EasyDataTable :headers="headers_dos" :items="bar_codes" show-index buttons-pagination
+      :server-items-length="total_bar_codes" :rows-items="[5, 10, 25, 30]" :rows-per-page="10" :search-field="nombre"
+      :search-value="search" :loading="loading">
+      <template #loading>
+        <img src="../assets/search.gif" />
+      </template>
+
+      <template #item-ticket="item" >
+        <div class="operation-wrapper manita">
+
+          <img src="../assets/pdf.png" class="operation-icon ml-1"
+            @click="generarArchivoConAlerta(item.nombre, item.gramaje, item.codigo)" width="30" height="30" />
+        </div>
+      </template>
+
+    </EasyDataTable>
+  </v-container>
+
+</template>
+
+<script>
+//import Loading from 'vue3-loading-overlay';
+
+//import 'vue3-loading-overlay/dist/vue3-loading-overlay.css';
+import Swal from 'sweetalert2';
+
+export default {
+  name: 'HelloWorld',
+
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      loading: false,
+      generando: false,
+      dialog: false,
+      search: '',
+      producto: '',
+      gramaje: '',
+      codigo: '',
+      cb: 'bar_codes',
+      test: 'http://192.168.1.78:88/cereales/site_php/wsObtenerArticulosLista.php',
+      //test: 'https://8752-189-177-185-226.ngrok-free.app/cereales/site_php/wsObtenerArticulosLista.php',
+      headers_dos: [
+        { text: 'Producto', value: 'nombre' },
+        { text: 'Gramaje', value: 'gramaje' },
+        { text: 'Codigo de barras', value: 'codigo' },
+        { text: 'Ticket', value: 'ticket' }
+      ],
+      bar_codes: [{"nombre":" AMARILLO HUEVO 170 100 GR","gramaje":"PZ","codigo":"0781159000492"}],
+      gases: [],
+      total_bar_codes: 0
+    }
+  },
+  methods: {
+    getBarCodes() {
+      this.loading = true;
+      this.$axios.get(this.test, {
+        "Content-Type": "application/json"
+
+      })
+        .then(response => {
+          this.bar_codes = response.data
+          this.total_bar_codes = response.data.length
+          //this.loading = false;
+        })
+        .catch(error => {
+          console.log(error)
+          //this.loading = false;
+        }).finally(() => {
+          this.loading = false;
+        });
+    },
+    modalPDF() {
+      this.generarArchivoConAlerta(this.producto, this.gramaje, this.codigo);
+    },
+    generarTicket(p_nombre, p_gramaje, p_codigo) {
+      this.generando = true;
+      this.$axios.post('http://192.168.1.78:3000/generate-pdf', {
+        nombre: p_nombre ? p_nombre : '',
+        gramaje: p_gramaje ? p_gramaje : '',
+        codigo: p_codigo ? p_codigo : ''
+      }, {
+        responseType: 'arraybuffer'
+      })
+        .then(response => {
+          this.dialog = false;
+          this.producto = '';
+          this.gramaje = '';
+          this.codigo = '';
+          const blob = new Blob([response.data], { type: 'application/pdf' });
+          const url = window.URL.createObjectURL(blob);
+
+          // Abre el PDF en una nueva pestaña
+          window.open(url, '_blank');
+          return Promise.resolve(response);
+        }, error => {
+          return Promise.reject(error);
+        })
+        .catch(error => {
+          console.log(error)
+        }).finally(() => {
+          this.generando = false;
+        });
+
+
+
+    },
+    async generarArchivoConAlerta(p_nombre, p_gramaje, p_codigo) {
+      // Mostrar SweetAlert2 con un mensaje de carga
+      const loadingAlert = Swal.fire({
+        title: 'Generando archivo',
+        text: 'Por favor, espera un momento...',
+        allowOutsideClick: false, // Evita que se cierre haciendo clic fuera del modal
+        showConfirmButton: false, // Oculta el botón de confirmación
+        willOpen: () => {
+          Swal.showLoading();
+        }
+      });
+
+      try {
+        // Generar el archivo
+        await this.generarTicket(p_nombre, p_gramaje, p_codigo);
+        // Cuando la generación del archivo es exitosa, cerrar la alerta
+        loadingAlert.close();
+        // Mostrar una alerta de éxito
+        Swal.fire({
+          icon: 'success',
+          title: 'Archivo generado',
+          text: 'El archivo se generó correctamente',
+          showConfirmButton: false,
+          timer: 1500
+        });
+      } catch (error) {
+        // Si hay un error durante la generación del archivo, mostrar una alerta de error
+        Swal.fire('Error', 'Hubo un problema al generar el archivo', 'error');
+      }
+    }
+  },
+  created() {
+    this.getBarCodes()
+  }
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+.manita {
+  cursor: pointer;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,44 @@
+
+import { createApp } from 'vue';
+import App from './App.vue';
+//import router from './router';
+//import store from './store';
+import Vue3EasyDataTable from 'vue3-easy-data-table';
+import 'vue3-easy-data-table/dist/style.css';
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+import axios from 'axios'
+
+
+
+
+
+const vuetify = createVuetify({
+    components,
+    directives,
+    icons: {
+        defaultSet: 'mdi',
+        aliases,
+        sets: {
+            mdi,
+        },
+    },
+
+})
+
+
+const app = createApp(App);
+
+//app.use(router);
+//app.use(store);
+app.use(vuetify);
+
+
+app.component('EasyDataTable', Vue3EasyDataTable);
+app.config.globalProperties.$axios = axios
+app.config.productionTip = false;
+app.config.globalProperties.$rutaBase = '/test/';
+app.mount('#app');
